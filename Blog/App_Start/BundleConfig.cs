6c0dadf5de5da4e4dfad0037f6d3ad4571364e81
kostadin <kostadin_terziev@abv.bg>
2016-12-06 16:36:22 +0200

    Adding the sidebar.

--- Blog/App_Start/BundleConfig.cs
+++ Blog/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace Blog
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/sidebar.css"));
         }
     }
 }
